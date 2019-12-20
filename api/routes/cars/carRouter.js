62d2e60259f49c09d96e8843f35e3e70ff937940
ndurbin <nick.durbin@gmail.com>
2019-12-20 04:50:02 -0500

    Initial boilerplate with proper imports

---
+++ api/routes/cars/carRouter.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../../../data/dbConfig')
+
+router.get('/', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
